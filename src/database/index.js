f4b3585ad21d5ac15cfa0bbf927cd10c8674d5b7
huycans <huycan_lk@yahoo.com.vn>
2021-07-07 16:16:05 -0400

    4-getListOfAgesOfUsersWith

--- src/database/index.js
+++ src/database/index.js
@@ -17,16 +17,46 @@ const mockDBCall = (dataAccessMethod) => {
 // MOCK DB CALLS
 //----------------
 const getUsers = () => {
-    const dataAccessMethod = () => _.map(db.usersById, userInfo => userInfo)
+    const dataAccessMethod = () => _.map(db.usersById, userInfo => userInfo);
     return mockDBCall(dataAccessMethod);
 };
 
 const getListOfAgesOfUsersWith = (item) => {
     const dataAccessMethod = () => {
-        // fill me in :)
-    }
+        //should return an arry of age count based on item.
+        //assume that every user has a different name
+
+        let usersWithItem = [];
+
+        //filter out users who do not have item
+        for (let user in db.itemsOfUserByUsername) {
+            if (db.itemsOfUserByUsername[user].indexOf(item) !== -1) {
+                usersWithItem.push(user);
+            }
+        }
+
+        //count the age of the users with item
+        let ageCount = {};
+        for (let userId in db.usersById) {//for every user info object in db
+            const userInfo = db.usersById[userId];
+            if (usersWithItem.indexOf(userInfo.username) !== -1) {//if user has the same name as user with item
+                if (!ageCount[userInfo.age]) {//and if user's age group has not been assigned yet
+                    ageCount[userInfo.age] = 1;
+                }
+                else {//found the age group of this user
+                    ageCount[userInfo.age] += 1;
+                }
+            }
+        }
+
+        //convert ageCount object to array
+        let result = [];
+        for (let age in ageCount) result.push({ age: +age, count: ageCount[age] });
+
+        return result;
+    };
     return mockDBCall(dataAccessMethod);
-}
+};
 
 module.exports = {
     getUsers,
