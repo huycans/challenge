1788073190eb1f182def82a73a515bd3ed7a20c1
huycans <huycan_lk@yahoo.com.vn>
2021-07-07 17:11:22 -0400

    Serve static files

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 const registerRoutes = require('./routes');
+const path = require("path");
+const cors = require("cors");
 
 // server config
 const port = process.env.PORT || 3000;
@@ -15,12 +17,17 @@ const start = () => {
         // start the server
         app.listen(port, () => {
             console.log(`Connected to Port ${port}`);
-            resolve()
+            resolve();
+        });
+        app.use(cors());
+        app.use(express.static(path.resolve(__dirname, '../public')));
+        app.get('/', function (req, res) {
+            res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
         });
     }).catch((error) => {
-        console.log(`failed to start server => ${error.message}`)
+        console.log(`failed to start server => ${error.message}`);
     });
-}
+};
 
 module.exports = start;
 
