a3b5967d8a4194725cdaeea0328b0a85324e8f03
huycans <huycan_lk@yahoo.com.vn>
2021-07-08 11:52:48 -0400

    Add loading message

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -8,31 +8,41 @@ import "./styles/App.scss";
 const App = function () {
   let [userInfo, setUserInfo] = useState(null);
   let [items, setItems] = useState(null);
+  let [loading, setLoading] = useState(false);
+
   let [selectedItem, setSelectedItem] = useState("Item"); //Item is default value
   let [itemsCountByAge, setItemsCountByAge] = useState(null);
 
   //handle when user select a item
   const handleSelect = (item: string) => {
+    setLoading(true);
     setSelectedItem(item);
     getListOfAgesOfUsersWithItem(item).then((ageGroups) => {
       setItemsCountByAge(ageGroups);
+      setLoading(false);
     });
   };
 
   //fetch data when the app start up, this will happen only once
   useEffect(() => {
+    setLoading(true);
     Promise.all([getUsers(), getItems()])
       .then(([users, items]) => {
         setUserInfo(users);
         setItems(items);
+        setLoading(false);
       })
       .catch((error) => {
         console.log("Cannot get data. Error: ", error);
+        setLoading(false);
       });
   }, []);
 
   return (
     <div className="app container">
+      <div className="row text-center">
+        {loading ? <h4 className="col">Please wait...</h4> : null}
+      </div>
       <UserInfo userInfo={userInfo}></UserInfo>
       <ItemCount
         handleSelect={handleSelect}
